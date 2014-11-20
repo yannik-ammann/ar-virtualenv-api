4a95869a539336b307a60b57e54ed76f1de33ff4
Yannik Ammann <ammann@arteria.ch>
2014-11-20 10:23:06 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import find_packages, setup
+import virtualenvapi
 
-version = arimagebucket.__version__
+version = virtualenvapi.__version__
 
 setup(
     name='ar-virtualenv-api',
